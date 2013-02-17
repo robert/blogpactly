5337b7c5834320453b248d98f889e3cfc67beaa4
John Ngo <john@goodafternoon.com.au>
2013-02-17 16:57:32 +0000

    Added basic development seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+creditor = User.create(email: 'doyle@brunson.com', password: 'supersystem')
+debtor = User.create(email: 'howard@lederer.com', password: 'theprofessor')
+pact = Pact.create(users: [creditor, debtor], created_at: 10.weeks.ago)
+
+Debt.create(pact: pact, amount: 5, creditor: creditor, debtor: debtor, marked_as_paid_by_creditor: true, marked_as_paid_by_debtor: true)
+Debt.create(pact: pact, amount: 5, creditor: debtor, debtor: creditor)
